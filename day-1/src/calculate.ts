5039c03ddd6a69f2d6414ede8110d3b41b636ea8
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-03-10 21:17:52 -0700

    Refactor calculateOrderTotal function to include input validation and return detailed breakdown

--- day-1/src/calculate.ts
+++ day-1/src/calculate.ts
@@ -1,9 +1,27 @@
-function calculateOrderTotal(price: number, quantity: number, taxRate: number): Number {
-    const subTotal: number = price * quantity;
-    const tax: number = subTotal * taxRate;
-    return subTotal + tax;
+function calculateOrderTotal(price: number, quantity: number, taxRate: number): object {
+
+  if (price < 0) {
+    throw new Error("Invalid price")
+  }
+
+  if (quantity < 0) {
+    throw new Error("Invalid quantity")
+  }
+
+  if (taxRate < 0) {
+    throw new Error("Invalid tax rate")
+  }
+
+  const subTotal = price * quantity
+  const tax = subTotal * taxRate
+
+  return {
+    subTotal: subTotal,
+    tax: tax.toFixed(2),
+    total: subTotal + tax
+  }
 }
 
-const total = calculateOrderTotal(50, 3, 0.07);
+const total = calculateOrderTotal(50, 3, 0.07)
 
-console.log(total);
+console.log(total)
