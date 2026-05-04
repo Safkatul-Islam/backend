bfd11de9f3eca4294a225c2a8da828fe6cedfc7f
Md Safkatul Islam <mdsafkatul.stu24@gmail.com>
2026-05-03 20:14:09 -0700

    Add fetchOrder function to retrieve products based on user ID

--- day-3/src/async.ts
+++ day-3/src/async.ts
@@ -3,6 +3,11 @@ interface User {
     name: string
 }
 
+interface order {
+    id: number,
+    product: string
+}
+
 async function fetchUser() {
 
     // dummy data
@@ -19,10 +24,29 @@ async function fetchUser() {
     })
 }
 
+async function fetchOrder(userId:number) {
+    
+    // dummy data
+    const products = [
+        { id: 1, product: "Keyboard" },
+        { id: 2, product: "Mouse" },
+        { id: 3, product: "Monitor" },
+        { id: 4, product: "Speaker" },
+        { id: 5, product: "Printer" },
+    ];
+
+    return new Promise((resolve) => {
+        resolve(products.filter(product => product.id === userId)
+                        .map(product => ({ id: product.id, product: product.product })));
+    })
+}
+
 async function getUserData() {
   const user = await fetchUser()
+  const product = await fetchOrder(3);
 
   console.log(user)
+  console.log(product)
 }
 
 getUserData();
